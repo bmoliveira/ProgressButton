26ca1c7d5120238810cf4322bd6866f3dea2cab2
Bruno Oliveira <bm.oliveira.dev@gmail.com>
2017-12-06 15:15:55 +0000

    Initial Project setup

---
+++ example-app/src/main/kotlin/com/broliveira/view/example/ExampleActivity.kt
@@ -0,0 +1,13 @@
+package com.broliveira.view.example
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.broliveira.view.R
+
+class ExampleActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_example)
+  }
+}
