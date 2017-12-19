fbb1fe9e6dcaeed1d05965d8593e6b8692f93df8
Bruno Oliveira <bm.oliveira.dev@gmail.com>
2017-12-19 01:38:13 +0000

    Added Progress button + helpers to create drawables

--- example-app/src/main/kotlin/com/broliveira/view/example/ExampleActivity.kt
+++ example-app/src/main/kotlin/com/broliveira/view/example/ExampleActivity.kt
@@ -2,12 +2,20 @@ package com.broliveira.view.example
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.broliveira.view.R
+import kotlinx.android.synthetic.main.activity_example.*
 
 class ExampleActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_example)
+  }
+
+  override fun onResume() {
+    super.onResume()
+    val drawable = mainButton.background
+    Log.e("AAA", "$drawable")
   }
 }
--- progress-button/src/main/kotlin/com/broliveira/view/ProgressButton.kt
+++ progress-button/src/main/kotlin/com/broliveira/view/ProgressButton.kt
@@ -1,15 +1,11 @@
 package com.broliveira.view
 
 import android.content.Context
-import android.graphics.PorterDuff
 import android.graphics.Typeface
 import android.graphics.drawable.Drawable
-import android.graphics.drawable.RippleDrawable
-import android.os.Build
 import android.support.v4.content.ContextCompat
 import android.support.v4.view.ViewCompat
 import android.util.AttributeSet
-import android.view.ContextThemeWrapper
 import android.view.Gravity
 import android.view.View
 import android.widget.FrameLayout
@@ -17,6 +13,7 @@ import android.widget.ProgressBar
 import android.widget.RelativeLayout
 import android.widget.TextView
 import org.jetbrains.anko.*
+
 
 class ProgressButton(
     context: Context,
@@ -28,100 +25,127 @@ class ProgressButton(
   constructor(context: Context, attrs: AttributeSet?): this(context, attrs, 0 , 0)
   constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int): this(context, attrs, defStyleAttr, 0)
 
-  var title: CharSequence = ""
+  var title: CharSequence = "Sign In"
     set(value) {
       titleTextView.text = value
       field = value
     }
 
-  var textColorResId: Int = R.color.white
+  var titleColor: Int = ContextCompat.getColor(context, android.R.color.white)
     set(value) {
-      titleTextView.textColor = ContextCompat.getColor(context, value)
-      progressView.setColorResId(context, value)
+      titleTextView.textColor = value
+      progressView.setColor(value)
       field = value
     }
 
-  var textSizeSp: Int = context.sp(18)
+  var titleTypeFace: Typeface = Typeface.create("sans-serif-medium", Typeface.NORMAL)
     set(value) {
-      titleTextView.textSize = context.sp(18).toFloat()
+      titleTextView.typeface = value
       field = value
     }
 
-  var isLoading: Boolean = false
+  var titleTextSize: Float = context.sp(9).toFloat()
+    set(value) {
+      titleTextView.textSize = value
+      field = value
+    }
+
+  var rippleBackgroundColor = ContextCompat.getColor(context, android.R.color.holo_red_dark)
+    set(value) {
+      field = value
+      baseLayout.background = defaultBackgroundDrawable
+    }
+
+  var rippleSecondaryColor = ContextCompat.getColor(context, android. R.color.holo_red_light)
+    set(value) {
+      field = value
+      baseLayout.background = defaultBackgroundDrawable
+    }
+
+  var minHeight: Int = dip(42)
+    set(value) {
+      titleTextView.minHeight = value
+      field = value
+    }
+
+  private val maxWidth: Int = Integer.MAX_VALUE
+  get() {
+    return field - internalMargin*2
+  }
+
+  var buttonElevation: Float = context.dip(4).toFloat()
+    set(value) {
+      ViewCompat.setElevation(baseLayout, value)
+      field = value
+    }
+
+  var isLoading: Boolean = true
     set(value) {
       progressView.visibility = if (value) { View.VISIBLE } else { View.INVISIBLE }
       field = value
     }
 
-  var buttonBacgroundDrawable: Drawable? = defaultBackgroundDrawable
-  set(value) {
-    backgroundDrawable = value
-    field = value
-  }
+  private val internalMargin = dip(5)
 
   private val defaultBackgroundDrawable: Drawable?
-  get() {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-      ContextCompat.getDrawable(context, R.drawable.main_ripple)
-    } else {
-      ContextCompat.getDrawable(context, R.drawable.main_ripple_compat)
-    }
-  }
+    get() = DrawableHelper.getSelectableDrawableFor(rippleBackgroundColor, rippleSecondaryColor, dip(30).toFloat())
 
   private lateinit var titleTextView: TextView
 
   private lateinit var progressView: ProgressBar
 
-  val baseLayout = maxWidthRelativeLayout(dip(320)) {
+  val baseLayout = maxWidthRelativeLayout(maxWidth) {
     clipToPadding = false
 
     titleTextView = textView {
-      typeface = Typeface.create("sans-serif-medium", Typeface.NORMAL)
-      textSize = this.textSize
       textAlignment = View.TEXT_ALIGNMENT_CENTER
-      textSize = 18f
-      textColor = ContextCompat.getColor(context, R.color.white)
-    }.lparams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT) {
-      centerInParent()
+      gravity = Gravity.CENTER
+    }.lparams(width = RelativeLayout.LayoutParams.MATCH_PARENT, height = RelativeLayout.LayoutParams.WRAP_CONTENT) {
+      centerHorizontally()
+      centerVertically()
+      marginStart = dip(35)
+      marginEnd = dip(35)
+      topPadding = dip(10)
+      bottomPadding = dip(10)
     }
 
     progressView = progressBar {
-      this.setColorResId(context, R.color.white)
-    }.lparams {
+      setColor(titleColor)
+    }.lparams(dip(30), dip(30)){
       alignParentRight()
-      alignParentTop()
-      alignParentBottom()
-      topMargin = dip(15)
-      bottomMargin = dip(15)
-      rightMargin = dip(0)
+      centerVertically()
+      rightPadding = dip(10)
     }
   }
 
   init {
-    clipToPadding = false
-    padding = dip(10)
     isClickable = true
 
     progressView.isIndeterminate = true
     progressView.visibility = View.INVISIBLE
 
-    val params = LayoutParams(LayoutParams.MATCH_PARENT, dip(54)).apply {
-      gravity = Gravity.CENTER
-    }
-    baseLayout.layoutParams = params
+    val params = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
+        .apply {
+          gravity = Gravity.CENTER
+          margin = internalMargin
+        }
 
-    ViewCompat.setElevation(baseLayout, context.dip(4).toFloat())
+    baseLayout.layoutParams = params
+    updateValues()
+  }
+
+  private fun updateValues() {
+    this.title = this.title
+    this.titleColor = this.titleColor
+    this.titleTypeFace = this.titleTypeFace
+    this.titleTextSize = this.titleTextSize
+    this.rippleBackgroundColor = this.rippleBackgroundColor
+    this.rippleSecondaryColor = this.rippleSecondaryColor
+    this.minHeight = this.minHeight
+    this.buttonElevation = this.buttonElevation
+    this.isLoading = this.isLoading
+    this.invalidate()
+    this.requestLayout()
   }
 }
-
-fun defaultRippleDrawable(): RippleDrawable {
-  return RippleDrawable(
-
-  )
-}
-
-fun defaultButtonDrawable(): Drawable {
-
-}
-
 
